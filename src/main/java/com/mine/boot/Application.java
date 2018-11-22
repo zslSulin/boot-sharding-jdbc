e7918796e2b5f788d71df443d67d5c535f9a3944
张苏林 <zhangsl@51jnq.com>
2018-11-22 19:24:14 +0800

    pom文档更改

--- src/main/java/com/mine/boot/Application.java
+++ src/main/java/com/mine/boot/Application.java
@@ -1,9 +1,14 @@
 package com.mine.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableTransactionManagement(proxyTargetClass = true)
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/mine/boot/config/DataSourceConfig.java
@@ -0,0 +1,15 @@
+package com.mine.boot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * DataSourceConfig
+ *
+ * @author zhangsl
+ * @date 2018/11/22 14:53
+ */
+@Configuration
+public class DataSourceConfig {
+
+
+}
