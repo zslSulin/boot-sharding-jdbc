c829b2c7d46b0cc38aa7a8724b7df92e8cf92cfe
张苏林 <zhangsl@51jnq.com>
2018-12-11 20:28:45 +0800

    redis 分布式锁

---
+++ src/main/java/com/mine/boot/config/redis/RedisLockAdvice.java
@@ -0,0 +1,76 @@
+package com.mine.boot.config.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.ThreadUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.lang3.reflect.MethodUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.lang.reflect.Method;
+
+/**
+ * RedisLockAdvice
+ *
+ * @author zhangsl
+ * @date 2018/12/11 16:57
+ */
+@Slf4j
+@Component
+@Aspect
+public class RedisLockAdvice {
+
+    @Resource
+    private RedisDistributionLock redisDistributionLock;
+
+    @Around("@annotation(RedisLockAnnotation)")
+    public Object processAround(ProceedingJoinPoint pjp) throws Throwable {
+        //获取方法上的注解对象
+        String methodName = pjp.getSignature().getName();
+        Class<?> classTarget = pjp.getTarget().getClass();
+        Class[] par = ((MethodSignature) pjp.getSignature()).getParameterTypes();
+        Method objMethod = classTarget.getMethod(methodName, par);
+        RedisLockAnnotation redisLockAnnotation = objMethod.getDeclaredAnnotation(RedisLockAnnotation.class);
+
+        //拼装分布式锁的key
+        String[] keys = redisLockAnnotation.keys();
+        Object[] args = pjp.getArgs();
+        Object arg = args[0];
+        StringBuilder temp = new StringBuilder();
+        temp.append(redisLockAnnotation.keyPrefix());
+        for (String key : keys) {
+            String getMethod = "get" + StringUtils.capitalize(key);
+            temp.append(MethodUtils.invokeExactMethod(arg, getMethod)).append("_");
+        }
+        String redisKey = StringUtils.removeEnd(temp.toString(), "_");
+
+        //执行分布式锁的逻辑
+        if (redisLockAnnotation.isSpin()) {
+            //阻塞锁
+            int lockRetryTime = 0;
+            try {
+                while (!redisDistributionLock.lock(redisKey, redisLockAnnotation.expireTime())) {
+                    if (lockRetryTime++ > redisLockAnnotation.retryTimes()) {
+                        log.error("lock exception. key:{}, lockRetryTime:{}", redisKey, lockRetryTime);
+                        throw new RuntimeException();
+                    }
+                }
+                return pjp.proceed();
+            } finally {
+                redisDistributionLock.unlock(redisKey);
+            }
+        } else {
+            //非阻塞锁
+            try {
+
+            } finally {
+                redisDistributionLock.unlock(redisKey)
+            }
+        }
+    }
+}
---
+++ src/main/java/com/mine/boot/config/redis/RedisLockAnnotation.java
@@ -0,0 +1,46 @@
+package com.mine.boot.config.redis;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * RedisLockAnnotation
+ *
+ * @author zhangsl
+ * @date 2018/12/11 16:52
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RedisLockAnnotation {
+
+    String keyPrefix() default "";
+
+    /**
+     * 要锁定的key中包含的属性
+     */
+    String[] keys() default {};
+
+    /**
+     * 是否阻塞锁
+     * 1. true:获取不到锁,阻塞一定时间;
+     * 2. false:获取不到锁,立即返回
+     */
+    boolean isSpin() default true;
+
+    /**
+     * 超出时间
+     */
+    int expireTime() default 10000;
+
+    /**
+     * 等待时间
+     */
+    int waitTime() default 50;
+
+    /**
+     * 获取不到锁的等待时间
+     */
+    int retryTimes() default 20;
+}
