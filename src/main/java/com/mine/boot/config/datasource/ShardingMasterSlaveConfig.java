b331b96a1a8d6a4dad8bbb3499600c114a032de9
张苏林 <zhangsl@51jnq.com>
2019-01-17 15:41:41 +0800

    读写分离配置取消

--- src/main/java/com/mine/boot/config/datasource/ShardingMasterSlaveConfig.java
+++ src/main/java/com/mine/boot/config/datasource/ShardingMasterSlaveConfig.java
@@ -1,26 +1,26 @@
-package com.mine.boot.config.datasource;
-
-import com.zaxxer.hikari.HikariDataSource;
-import io.shardingsphere.core.api.config.MasterSlaveRuleConfiguration;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * ShardingMasterSlaveConfig
- *
- * @author zhangsl
- * @date 2018/12/13 11:58
- */
-@Getter
-@Setter
-@ConfigurationProperties(prefix = "sharding.jdbc")
-public class ShardingMasterSlaveConfig {
-
-    private Map<String, HikariDataSource> dataSources = new HashMap<>();
-
-    private MasterSlaveRuleConfiguration masterSlaveRule;
-}
+//package com.mine.boot.config.datasource;
+//
+//import com.zaxxer.hikari.HikariDataSource;
+//import io.shardingsphere.core.api.config.MasterSlaveRuleConfiguration;
+//import lombok.Getter;
+//import lombok.Setter;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * ShardingMasterSlaveConfig
+// *
+// * @author zhangsl
+// * @date 2018/12/13 11:58
+// */
+//@Getter
+//@Setter
+//@ConfigurationProperties(prefix = "sharding.jdbc")
+//public class ShardingMasterSlaveConfig {
+//
+//    private Map<String, HikariDataSource> dataSources = new HashMap<>();
+//
+//    private MasterSlaveRuleConfiguration masterSlaveRule;
+//}
